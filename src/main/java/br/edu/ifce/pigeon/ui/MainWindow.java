699e5f7c0c0bea25e33e2d35cb20af7e18bd2b5b
JMateusSousa <mateus_sousa_peu@hotmail.com>
2019-04-02 10:08:02 -0300

    nothing

--- src/main/java/br/edu/ifce/pigeon/ui/MainWindow.java
+++ src/main/java/br/edu/ifce/pigeon/ui/MainWindow.java
@@ -15,7 +15,6 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.awt.image.BufferedImage;
